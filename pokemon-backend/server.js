4a4f2610990d99a3b28c3fea5e8229ac8de0ab0b
iqbal024 <muhammadiqbalalikhadafi@gmail.com>
2024-07-13 11:52:18 +0700

    install hope for expressjs

--- pokemon-backend/server.js
+++ pokemon-backend/server.js
@@ -1,40 +1,73 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(cors());
+const Hapi = require("@hapi/hapi");
+const cors = require("hapi-cors");
 
-const isPrime = (num) => {
-  if (num <= 1) return false;
-  if (num <= 3) return true;
-  if (num % 2 === 0 || num % 3 === 0) return false;
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
-  return true;
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  await server.register({
+    plugin: cors,
+    options: {
+      origins: ["*"],
+    },
+  });
+
+  const isPrime = (num) => {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  };
+
+  let renameCount = 0;
+  const fibonacci = (num) => {
+    if (num <= 1) return num;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  };
+
+  // REST API to return 50% probability for catching Pokemon
+  server.route({
+    method: "GET",
+    path: "/catch",
+    handler: (request, h) => {
+      const success = Math.random() < 0.5;
+      return { success };
+    },
+  });
+
+  // REST API to release Pokemon
+  server.route({
+    method: "GET",
+    path: "/release",
+    handler: (request, h) => {
+      const number = Math.floor(Math.random() * 100);
+      return { success: isPrime(number) };
+    },
+  });
+
+  // REST API to rename Pokemon
+  server.route({
+    method: "GET",
+    path: "/rename",
+    handler: (request, h) => {
+      renameCount++;
+      const newName = `${request.query.name}-${fibonacci(renameCount)}`;
+      return { rename: newName };
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-let renameCount = 0;
-const fibonacci = (num) => {
-  if (num <= 1) return num;
-  return fibonacci(num - 1) + fibonacci(num - 2);
-};
-
-app.get("/catch", (req, res) => {
-  const success = Math.random() < 0.5;
-  res.json({ success });
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
-app.get("/release", (req, res) => {
-  const number = Math.floor(Math.random() * 100);
-  res.json({ success: isPrime(number) });
-});
-
-app.get("/rename", (req, res) => {
-  renameCount++;
-  const newName = `${req.query.name}-${fibonacci(renameCount)}`;
-  res.json({ rename: newName });
-});
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+init();
